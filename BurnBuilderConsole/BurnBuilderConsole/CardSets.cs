d97bdf0ea532e1b437f96f5a6872cf2a10807356
Darren Ross <darren.ross@bellevuecollege.edu>
2020-11-09 15:39:07 -0800

    Added crap code for us all to see.

---
+++ BurnBuilderConsole/BurnBuilderConsole/CardSets.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Transactions;
+using MtgApiManager.Lib.Model;
+
+namespace BurnBuilderConsole
+{
+    public class Sets
+    {
+        public Set SetData { get; set; }
+    }
+    
+    public class CardSet
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+        
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("booster")]
+        public string[] Booster { get; set; }
+
+        [JsonPropertyName("releaseDate")]
+        public DateTime ReleaseDate { get; set; }
+
+        [JsonPropertyName("block")]
+        public string Block { get; set; }
+
+        [JsonPropertyName("onlineOnly")]
+        public string OnlineOnly { get; set; }
+
+    }
+
+    public class CardSets : IEnumerable
+    {
+        private CardSet[] _cardSets;
+
+        public CardSets(CardSet[] cSArray)
+        {
+            _cardSets = new CardSet[cSArray.Length];
+
+            for (int i = 0; i < cSArray.Length; i++)
+            {
+                _cardSets[i] = cSArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public CardSetsEnum GetEnumerator()
+        {
+            return new CardSetsEnum(_cardSets);
+        }
+    }
+
+    public class CardSetsEnum : IEnumerator
+    {
+        public CardSet[] _cardSets;
+
+        int position = -1;
+
+        public CardSetsEnum(CardSet[] list)
+        {
+            _cardSets = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _cardSets.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public CardSet Current
+        {
+            get
+            {
+                try
+                {
+                    return _cardSets[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
--- BurnBuilderConsole/BurnBuilderConsole/Program.cs
+++ BurnBuilderConsole/BurnBuilderConsole/Program.cs
@@ -1,12 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using MtgApiManager;
+using MtgApiManager.Lib.Core;
+using MtgApiManager.Lib.Model;
+using MtgApiManager.Lib.Service;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serializers.SystemTextJson;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace BurnBuilderConsole
 {
     class Program
     {
-        static void Main(string[] args)
+
+        // Create the HTTP Client to reach out and access the api.
+        //private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            await ProcessSets();
+            Console.WriteLine("******************************************");
+            Console.WriteLine("********* This is a new method! **********");
+            Console.WriteLine("******************************************");
+
+            //await ProcessApiSets();
+
+        }
+
+        // Access Methods to get JSON data.
+        private static async Task ProcessSets()
+        {
+            // HTTP Call headers
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/api.magicthegathering.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", "Burn Builder");
+
+            var client = new RestClient("https://api.magicthegathering.io/v1");
+            client.UseSystemTextJson();
+            var request = new RestRequest("sets", DataFormat.Json);
+
+            IRestResponse response = client.Get<CardSet>(request);
+
+            Console.WriteLine("Response Data");
+            Console.WriteLine(response.Content);
+
+            Set cardSets = JsonSerializer.Deserialize<Set>(response.Content, new JsonSerializerOptions {AllowTrailingCommas = true});
+
+            foreach (var cardSet in cardSets)
+            {
+                Console.WriteLine(cardSet.Name);
+            }
+
+            // This is the api uri to call and get data from API.
+            //var streamTask = client.GetStreamAsync("https://api.magicthegathering.io/v1/sets");
+            // Deserialize the data Stream from the API.
+            //var sets = await JsonSerializer.DeserializeAsync<Sets>(await streamTask);
+
+            //CardSets cardSets = JsonSerializer.Deserialize<Sets(response)
+
+            //// Set up the service call, and allow for error gathering.
+            //SetService service = new SetService();
+            //Exceptional<List<Set>> result = service.All();
+            //if (result.IsSuccess)
+            //{
+            //    var value = result.Value;
+            //}
+            //else
+            //{
+            //    var exception = result.Exception;
+            //}
+            //// If needed for exception.
+            //Console.WriteLine(result.Value);
+            //Console.WriteLine(result.Exception);
+
+            // save the call data to a variable when returned.
+            //foreach (var set in sets)
+            //{
+            //    Console.WriteLine(set.Name);
+            //}
+        }
+
+        private static async Task ProcessApiSets()
+        {
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", "Burn Builder");
+
+            //var stringTask = client.GetStringAsync("https://api.magicthegathering.io/v1/sets");
+
+            //string setJson = stringTask.ToString();
+
+            
+
+            //CardService service = new CardService();
+            //Exceptional<List<Card>> result = service.All();
+            //if (result.IsSuccess)
+            //{
+            //    var value = result.Value;
+            //}
+            //else
+            //{
+            //    var exception = result.Exception;
+            //}
+            //Console.WriteLine(result.Value);
+            //Console.WriteLine(result.Exception);
+
+            //var asyncResult = await stringTask;
+
+            //Console.WriteLine(asyncResult);
+        }
+
+        private static async Task ProcessCards()
+        {
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/api.magicthegathering.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", "Burn Builder");
+
+            //var stringTask = client.GetStringAsync("https://api.magicthegathering.io/v1/cards");
+
+            //CardService service = new CardService();
+            //Exceptional<List<Card>> result = service.All();
+            //if (result.IsSuccess)
+            //{
+            //    var value = result.Value;
+            //}
+            //else
+            //{
+            //    var exception = result.Exception;
+            //}
+            //Console.WriteLine(result.Value);
+            //Console.WriteLine(result.Exception);
+
+            //var asyncResult = await stringTask;
+
+            //Console.WriteLine(asyncResult);
         }
     }
 }
