f9044d1b3f1c057b0e1dc163b25d94c9a2b93fca
Khang Tran <codlover775@gmail.com>
2020-11-12 18:57:24 -0800

    first commit

---
+++ BurnBuilder/BurnBuilder/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurnBuilder.Models
+{
+    public class UserInfo
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+}
