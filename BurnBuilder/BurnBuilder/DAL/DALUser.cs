a0b10a636fefe8cd87a918127050636a8c6607a4
Darren Ross <darren.ross@bellevuecollege.edu>
2020-11-23 16:39:15 -0800

    Created DAL code for User.

---
+++ BurnBuilder/BurnBuilder/DAL/DALUser.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using BurnBuilder.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BurnBuilder.DAL
+{
+    public class DALUser
+    {
+        private readonly IConfiguration configuration;
+
+        public DALUser(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        internal string InsertUser(Models.User user)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string query = "INSERT INTO [dbo].[User]([EmailAddress],[Password],[FirstName],[LastName],[Created])" + " VALUES (@EmailAddress,@Password,@FirstName,@LastName,@Created) select SCOPE_IDENTITY() as id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", user.LastName);
+            cmd.Parameters.AddWithValue("@Created", user.Created);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            string UserID = reader[0].ToString();
+
+            reader.Close();
+
+            conn.Close();
+            
+            return UserID;
+        }
+
+        internal User GetUserById(int userId)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string query = "SELECT [EmailAddress], [Password], [FirstName], [LastName], [Created] FROM [dbo].[User] WHERE UserID = @UserID";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@UserID", userId);
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            string emailAddress = reader["EmailAddress"].ToString();
+            string password = reader["Password"].ToString();
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            DateTime created = Convert.ToDateTime(reader["Created"].ToString());
+
+            reader.Close();
+
+            conn.Close();
+
+            User u = new User();
+            u.EmailAddress = emailAddress;
+            u.Password = password;
+            u.FirstName = firstName;
+            u.LastName = lastName;
+            u.Created = created;
+
+            return u;
+        }
+
+        internal User UpdateUser(Models.User user)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string query = "UPDATE [dbo].[User] SET [EmailAddress] = @EmailAddress, [Password] = @Password, [FirstName] = @FirstName, [LastName] = @LastName, [Created] = @Created WHERE UserID = @UserID";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", user.LastName);
+            cmd.Parameters.AddWithValue("@Created", user.Created);
+            cmd.Parameters.AddWithValue("@UserID", user.UserId);
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            User u = new User();
+            
+            return u;
+        }
+
+        internal void DeleteUser(int userId)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string query = "DELETE [dbo].[User] WHERE UserID = @UserID";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@UserID", userId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        // May implement a loginCredentials Model for this as it is simple and more secure.
+        internal User IsValidUser(User user)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string query = "SELECT UserID, FirstName FROM User WHERE EmailAddress = @EmailAddress AND Password = @Password";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@EmailAddress", user.EmailAddress);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            User u = null;
+
+            if (reader.Read())
+            {
+                u = new User();
+                u.FirstName = reader["FirstName"].ToString();
+                u.UserId = Int32.Parse(reader["UserID"].ToString());
+            }
+            else
+            {
+                // Need to figure out to return something if they are not valid users. Maybe in version 1.1.
+            }
+
+            reader.Close();
+
+            conn.Close();
+            
+            return u;
+        }
+    }
+}
