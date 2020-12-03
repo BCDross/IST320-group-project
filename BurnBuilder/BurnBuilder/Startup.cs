dfcc6a78f3a913f94a64f23a14c7b6a6798da536
Devon <55559667+K1ngsJester@users.noreply.github.com>
2020-12-02 17:50:25 -0800

    Update Startup.cs
    added line 37 and 38 to allow the session to be created while using the program

--- BurnBuilder/BurnBuilder/Startup.cs
+++ BurnBuilder/BurnBuilder/Startup.cs
@@ -34,6 +34,8 @@ namespace BurnBuilder
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddMvc().AddSessionStateTempDataProvider();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
