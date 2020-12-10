0ea45e5aaf62941e7de9de19e58972a149974f0b
khalidmohamed-j <64443948+khalidmohamed-j@users.noreply.github.com>
2020-12-09 21:44:40 -0800

    Update ErrorViewModel.cs

--- BurnBuilder/BurnBuilder/Models/ErrorViewModel.cs
+++ BurnBuilder/BurnBuilder/Models/ErrorViewModel.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace BurnBuilder.Models
 {
+    /// <summary>
+    /// This is the error properties. 
+    /// </summary>
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
