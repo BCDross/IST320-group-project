86b72ff6935efe0f2d9b0b093e34d15fde091146
khalidmohamed-j <64443948+khalidmohamed-j@users.noreply.github.com>
2020-12-09 21:36:08 -0800

    Update HomeController.cs

--- BurnBuilder/BurnBuilder/Controllers/HomeController.cs
+++ BurnBuilder/BurnBuilder/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace BurnBuilder.Controllers
             return View();
         }
         
+        /// <summary>
+        /// This registers a new user and sends a userID to the database.
+        /// </summary>
+        /// <param name="User"></param>
+        /// <returns></returns>
         public IActionResult Register(User User)
         {
             DALUser dALPerson = new DALUser(_configuration);
@@ -39,6 +44,12 @@ namespace BurnBuilder.Controllers
             return View(User);
         }
         
+        /// <summary>
+        /// Checks to see if UserID matches what is in database. 
+        /// It returns login in successful if it matches and login in failed it it does not find a userID that matches it. 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public IActionResult checkValidUser(User user)
         {
             DALUser dp = new DALUser(_configuration);
@@ -57,6 +68,10 @@ namespace BurnBuilder.Controllers
 
         }
         
+        /// <summary>
+        /// Displays Home page with user name if login was successful or Index page if login failed. 
+        /// </summary>
+        /// <returns></returns>
         public IActionResult HomePage()
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -87,6 +102,11 @@ namespace BurnBuilder.Controllers
             }
         }*/
 
+        /// <summary>
+        /// Displays user account if logged in or index page.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public IActionResult Account(int userId)
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -102,6 +122,11 @@ namespace BurnBuilder.Controllers
             }
         }
 
+        /// <summary>
+        /// Dsplays ViewCard page after login or index page.
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
         public IActionResult ViewCard(Card card)
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -118,6 +143,12 @@ namespace BurnBuilder.Controllers
                 return View(card);
             }
         }
+        
+        /// <summary>
+        /// Dsplays ViewDeck page after login or index page.
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <returns></returns>
         public IActionResult ViewDeck(Deck deck)
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -141,6 +172,12 @@ namespace BurnBuilder.Controllers
                 }
             }
         }
+        
+        /// <summary>
+        /// Dsplays BrowseCards page after login or index page.
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
         public IActionResult BrowseCards()
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -157,6 +194,12 @@ namespace BurnBuilder.Controllers
                 return View(cardList);
             }
         }
+        
+        /// <summary>
+        ///Dsplays BrowseDecks page after login or index page. 
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <returns></returns>
         public IActionResult BrowseDecks(Deck deck)
         {
             string struID = HttpContext.Session.GetString("uID");
@@ -185,6 +228,10 @@ namespace BurnBuilder.Controllers
             return View();
         }*/
         
+        /// <summary>
+        /// Traces reason for error and returns an error message. 
+        /// </summary>
+        /// <returns></returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
